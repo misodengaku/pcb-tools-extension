09cdb302d97a4ce32d18695932f8a384e0acd3fc
misodengaku <misodengaku@gmail.com>
2024-10-30 23:37:59 +0900

    wip

--- panelize.py
+++ panelize.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys, os, json
-from copy import deepcopy
 import gerberex
 from gerberex import DxfFile, GerberComposition, DrillComposition, PanelizeScript
 from gerberex.panelize_script import PanelizeItem
@@ -22,78 +21,66 @@ def putstr(text):
     sys.stdout.flush()
 
 
+def process_drill():
+    ctx = DrillComposition()
+    file = gerberex.read(ps.mousebite["dxf_path"])
+    file.draw_mode = DxfFile.DM_MOUSE_BITES
+    file.to_metric()
+    file.width = 0.5
+    file.format = (3, 3)
+    ctx.merge(file)
+    putstr("output mousebite")
+
+
 def process_gerber(
     item: PanelizeItem, ext: str, offset: tuple[int, int] = None
-) -> tuple[int, int]:
+) -> tuple[tuple[int, int], tuple[int, int] | None]:
     ctx = GerberComposition()
     for y_count in range(0, item.row):
         for x_count in range(0, item.column):
-            file = gerberex.read(item.path_prefix + ext)
+            file = gerberex.read(f"{item.path_prefix}.{ext}")
             file.to_metric()
             size = file.size
             base_offset_x = item.x
             base_offset_y = item.y
+            min_x = file.bounding_box[0][0]
+            min_y = file.bounding_box[1][0]
+            est_offset = (min_x, min_y)
             file.rotate(item.angle)
             if offset:
-                print("offset base: %.2fx%.2f" % offset)
                 file.offset(
                     base_offset_x + offset[0] * x_count,
                     base_offset_y + offset[1] * y_count,
                 )
             else:
                 file.offset(
-                    base_offset_x + size[0] * x_count,
-                    base_offset_y + size[1] * y_count,
+                    base_offset_x + min_x + size[0] * x_count,
+                    base_offset_y + min_y + size[1] * y_count,
                 )
             putstr("_")
             ctx.merge(file)
     putstr(".")
     ctx.dump(ps.output + "." + ext)
-    return size
+    return (size, est_offset)
 
 
 for v in ps.files:
     item = PanelizeItem(**v)
     putstr("merging outline(%s): " % item.outline_ext)
-    size = process_gerber(item, item.outline_ext)
+    size, est_offset = process_gerber(item, item.outline_ext)
     putstr("\n")
     print("single board size: %.2fx%.2f" % size)
+    print("estimated offset: %.2fx%.2f" % est_offset)
 
     for ext in ps.target_extensions:
         putstr("merging %s: " % ext)
 
         match ext:
             case "TXT":
-                ctx = DrillComposition()
-
-                if ps.mousebite and ps.mousebite["enable"] and ps.mousebite["dxf_path"]:
-                    file = gerberex.read(ps.mousebite["dxf_path"])
-                    file.draw_mode = DxfFile.DM_MOUSE_BITES
-                    file.to_metric()
-                    file.width = 0.5
-                    file.format = (3, 3)
-                    ctx.merge(file)
-                    putstr("output mousebite")
+                # if ps.mousebite and ps.mousebite["enable"] and ps.mousebite["dxf_path"]:
+                process_drill()
             case _:
-                # ctx = GerberComposition()
                 process_gerber(item, ext, size)
-                # item = PanelizeItem(**v)
-                # for y_count in range(0, item.row):
-                #     for x_count in range(0, item.column):
-                #         file = gerberex.read(item.path_prefix + ext)
-                #         file.to_metric()
-                #         size = file.size
-                #         base_offset_x = item.x
-                #         base_offset_y = item.y
-                #         file.rotate(item.angle)
-                #         file.offset(
-                #             base_offset_x + size[0] * x_count,
-                #             base_offset_y + size[1] * y_count,
-                #         )
-                #         putstr("_")
-                #         ctx.merge(file)
-                # putstr(".")
-                # ctx.dump(ps.output + "." + ext)
 
         putstr("\n")
 
